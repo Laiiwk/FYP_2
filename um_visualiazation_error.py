d34cae742141e23e9279efe45b69b2e5b6ff4e19
Laiiwk <wenkhaidev@gmail.com>
2024-03-14 01:57:51 +0800

    added new visualization for the error access, something removed

--- um_visualiazation_error.py
+++ um_visualiazation_error.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns 
 
 error_df = pd.read_csv('output/extracted_error_features.csv') 
 
@@ -20,9 +21,14 @@ daily_error_breakdown = error_df.groupby(['day', 'error_type_encoded']).size().u
 # plt.title('Top 10 Client IPs with Errors')
 # plt.show()
 
-pd.crosstab(error_df['error_file'], error_df['error_type_encoded']).plot(kind='bar', stacked=True) 
-plt.xlabel('Resource / File')
+error_df['error_file'].value_counts().head(15).plot(kind='bar')
+plt.xlabel('Error File')
 plt.ylabel('Error Count')
-plt.title('Error Distribution by Resource and Type')
+plt.title('Top 15 Error-Prone Resources')
+plt.xticks(rotation=90)  # Rotate labels if they overlap
 plt.show()
 
+ct = pd.crosstab(error_df['error_file'], error_df['error_type_encoded'])
+sns.heatmap(ct, cmap='viridis', annot=True)  # annot=True to display counts in cells 
+plt.title('Heatmap of Error Distribution')
+plt.show()
