15e0c1051a6ac247448374b91c123739dd3703b8
Laiiwk <wenkhaidev@gmail.com>
2024-04-18 11:32:12 +0800

    dropped more unused column, custom error level

--- um_error_feature_extract.py
+++ um_error_feature_extract.py
@@ -1,8 +1,7 @@
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
 
 def extract_error_features(input_path, output_path):
-    """Reads processed error logs, extracts features, and saves the enhanced DataFrame.
+    """Reads processed error logs, extracts features, maps error levels, and saves the enhanced DataFrame.
 
     Args:
         input_path (str): Path to the processed error input_data CSV file.
@@ -20,15 +19,23 @@ def extract_error_features(input_path, output_path):
     log_df['month'] = log_df['timestamp'].dt.month
     log_df['year'] = log_df['timestamp'].dt.year
 
+    # Error Level Mapping
+    error_level_map = {
+        "php7:notice": 1,        
+        "php7:warn": 2,  
+        "php7:error": 3  # Add more mappings if needed
+    }
+
+    def map_error_level(error_level):
+        return error_level_map.get(error_level, -1)  # -1 for unknown levels
+
+    log_df['error_level_encoded'] = log_df['error_level'].apply(map_error_level) 
+
     # Feature engineering
-    le = LabelEncoder()
-    log_df['error_level_encoded'] = le.fit_transform(log_df['error_level'])
-    log_df['error_type_encoded'] = le.fit_transform(log_df['error_type'])
-    log_df['error_message_length'] = log_df['error_message'].apply(len)
+    #log_df['error_message_length'] = log_df['error_message'].apply(len)
 
     # Drop redundant columns
-    log_df = log_df.drop(columns=['error_level', 'error_type', 'error_message','referer'])
-
+    log_df = log_df.drop(columns=['timestamp','error_level','error_type', 'error_message','referer']) 
     log_df.to_csv(output_path + 'extracted_error_features.csv', index=False)
 
 if __name__ == '__main__':
